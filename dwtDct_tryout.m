796f961b1d87928307ea183566e038659aabb72b
samuel <samuelp@uci.edu>
2019-03-10 23:05:19 -0700

    Extraction works when you change the mother wavelet to haar :)

--- dwtDct_tryout.m
+++ dwtDct_tryout.m
@@ -3,6 +3,7 @@ clear all; close all;
 % EMBEDDING WATERMARK
 
 filename = 'peppers.png';
+mother_wavelet = 'haar';
 image = double(imread(filename));
 figure(1)
 imshow(image/255);
@@ -33,10 +34,10 @@ green = image(:,:,2);
 blue = image(:,:,3); % We're only going to work with blue
 
 % Get the fourth-level DWT for blue sub-band
-[xab1, xhb1, xvb1, xdb1] = dwt2(blue, 'db2');
-[xab2, xhb2, xvb2, xdb2] = dwt2(xab1, 'db2');
-[xab3, xhb3, xvb3, xdb3] = dwt2(xab2, 'db2');
-[xab4, xhb4, xvb4, xdb4] = dwt2(xab3, 'db2');
+[xab1, xhb1, xvb1, xdb1] = dwt2(blue, mother_wavelet);
+[xab2, xhb2, xvb2, xdb2] = dwt2(xab1, mother_wavelet);
+[xab3, xhb3, xvb3, xdb3] = dwt2(xab2, mother_wavelet);
+[xab4, xhb4, xvb4, xdb4] = dwt2(xab3, mother_wavelet);
 
 % Use Discrete Cosine Transforows on B component
 % of fourth-level DWT
@@ -48,7 +49,7 @@ figure,imshow(xab4);
 
 % Add alpha*mask to the DCT of B
 [rows,cols] = size(xab4);
-DCT_blue_sum(1:rows,1:cols) = DCT_blue(1:rows,1:cols) + alpha * mask;
+DCT_blue_sum(1:rows,1:cols) = DCT_blue(1:rows,1:cols) + alpha * mask(1:rows,1:cols);
 
 figure(20),title('DCT_blue_sum'),imshow(DCT_blue_sum)
 
@@ -65,11 +66,13 @@ row3=193, col3=257;
 
 
 % Inverse DWT using the blue_INV
-Level3 = idwt2(blue_INV, xhb4, xvb4, xdb4, 'db2');
-Level2 = idwt2(Level3(1:row1,1:col1), xhb3, xvb3, xdb3, 'db2');
-Level1 = idwt2(Level2(1:row2,1:col2), xhb2, xvb2, xdb2, 'db2');
-Blue_Final = idwt2(Level1(1:row3,1:col3), xhb1, xvb1, xdb1, 'db2');
-
+Level3 = idwt2(blue_INV, xhb4, xvb4, xdb4, mother_wavelet);
+%Level2 = idwt2(Level3(1:row1,1:col1), xhb3, xvb3, xdb3, mother_wavelet);
+%Level1 = idwt2(Level2(1:row2,1:col2), xhb2, xvb2, xdb2, mother_wavelet);
+%Blue_Final = idwt2(Level1(1:row3,1:col3), xhb1, xvb1, xdb1, mother_wavelet);
+Level2 = idwt2(Level3, xhb3, xvb3, xdb3, mother_wavelet);
+Level1 = idwt2(Level2, xhb2, xvb2, xdb2, mother_wavelet);
+Blue_Final = idwt2(Level1, xhb1, xvb1, xdb1, mother_wavelet);
 
 % Put everything together
 final(:,:,1)=red;
@@ -81,24 +84,16 @@ figure, imshow(100 - abs(final-image)*100)
 % EXTRACTION
 
 % Get the fourth-level DWT for blue sub-band
-[exab1, exhb1, exvb1, exdb1] = dwt2(final(:,:,3), 'db2');
-[exab2, exhb2, exvb2, exdb2] = dwt2(exab1, 'db2');
-[exab3, exhb3, exvb3, exdb3] = dwt2(exab2, 'db2');
-[exab4, exhb4, exvb4, exdb4] = dwt2(exab3, 'db2');
+[exab1, exhb1, exvb1, exdb1] = dwt2(final(:,:,3), mother_wavelet);
+[exab2, exhb2, exvb2, exdb2] = dwt2(exab1, mother_wavelet);
+[exab3, exhb3, exvb3, exdb3] = dwt2(exab2, mother_wavelet);
+[exab4, exhb4, exvb4, exdb4] = dwt2(exab3, mother_wavelet);
 
 DCT_final = dct2(exab4);
 figure(21),title('DCT_final'),imshow(DCT_final)
-extracted_Mask = zeros(rows,cols);
 %extracted_Mask(1:rows,1:cols) = (DCT_final(1:rows,1:cols) - DCT_blue(1:rows,1:cols))/alpha;
-for r = 1:rows
-    for c = 1:cols
-        extracted_Mask(r, c) = (DCT_final(r, c) - DCT_blue(r, c))/alpha;
-        if extracted_Mask(r, c) > 1
-            extracted_Mask(r, c) = 1;
-        else
-            extracted_Mask(r, c) = 0;
-        end
-    end
-end
-figure(101),imshow(extracted_Mask)
+extracted_Mask = (DCT_final - DCT_blue)/alpha;
+figure(101),imshow(extracted_Mask);
+final_mask = round(extracted_Mask);
+
 
